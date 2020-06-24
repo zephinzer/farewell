a83bf4528d59f8946375488e4dc389a385b97501
Kai Wu Toh <kwtoh2000@hotmail.com>
2020-06-24 20:42:47 +0800

    Added messages

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -24,19 +24,26 @@ export const messages = [
         alt: "alt-example",
     },
     {
-        header: "Test header 2",
-        message: "Hello, this is a very very very very very very very very very very very very very medium message",
-    },
-    {
-        header: "Test long long long header 4",
-        message: "Hello, this is a very short message",
-    },
-    {
-        header: "Test header 5",
+        header: "Test header 6",
         message: "Hello, this is a very very very very very very very very very very very very very very very very very very very very very very very very very very long message",
     },
     {
-        header: "Test header 2",
-        message: "Hello, this is a very very very very very very very very very very very very very medium message",
+        message: "Hello, this is a very very very very very very very very very very very very very medium message with no header",
+    },
+    {
+        header: "Test header 7",
+        message: "Hello, this is a very very very very very very very very very very very very very very very very very very very very very very very very very very long message",
+    },
+    {
+        header: '"Test header 8"',
+        message: 'Hello, this is a very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very long message',
+    },
+    {
+        header: "Test long long long header 9",
+        message: "Hello, this is a very short message",
+    },
+    {
+        header: "Test header 10",
+        message: "Hello, this is a very very very very very very very very very very very very very very very very very very very very very very very very very very long message",
     },
 ];
