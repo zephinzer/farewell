3a641288e9903218790d28450feba0eab0d2f623
Alvin Leu <witxhhaven@gmail.com>
2020-06-27 10:15:06 +0800

    remove unused bgs and make face rotate

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "../App.css"
 
 export const Header = () => <div className="header-container">
-                                <h1 className="header"><i className="joeface"/> Bye bye Joseph! <i className="joeface"/>
+                                <h1 className="header"><i className="joeface-left"/> Bye bye Joseph! <i className="joeface-right"/>
                                 </h1>
                                 <p className="header-subtitle">Here's some parting messages and photos from the team... T_T</p>
                             </div>;
